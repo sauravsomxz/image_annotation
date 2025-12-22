248d9788a828036b237e4e54e53723f8845dcc0e
sauravsomxz <saurav.maharana07@gmail.com>
2025-12-22 15:15:25 +0530

    Generic File holding all String  used in-app

---
+++ lib/core/app_strings.dart
@@ -0,0 +1,12 @@
+// @author [Sourav Ranjan Maharana]
+// @email [saurav.maharana07@gmail.com]
+// @create date 17:10:39 2025-12-21
+// @modify date 17:10:39 2025-12-21
+// @desc [File holds all the Strings used in-app]
+
+class AppStrings {
+  static const String appName =
+      "Cambrian Bioworks: Assignment - Image Annotation";
+
+  static const String imageAnnotation = "Image Annotation";
+}
