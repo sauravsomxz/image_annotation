b45ba26df01b15b4c20f1f070b52716d4fe11fc7
sauravsomxz <saurav.maharana07@gmail.com>
2025-12-22 15:15:10 +0530

    Generic File holding all asset images used in-app

---
+++ lib/core/app_image_paths.dart
@@ -0,0 +1,14 @@
+// @author [Sourav Ranjan Maharana]
+// @email [saurav.maharana07@gmail.com]
+// @create date 16:56:37 2025-12-21
+// @modify date 16:56:37 2025-12-21
+// @desc [File holds all the asset image paths used in-app]
+
+class AppImagePaths {
+  static const _baseImagePath = "assets/images/";
+
+  static const String sampleImagePath1 = "${_baseImagePath}cellImage1.jpg";
+  static const String sampleImagePath2 = "${_baseImagePath}cellImage2.jpg";
+
+  static const listOfImages = [sampleImagePath1, sampleImagePath2];
+}
