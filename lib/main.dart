3a93d63eeeb4c5dfd452346d4d318440de551db7
sauravsomxz <saurav.maharana07@gmail.com>
2025-12-24 00:17:54 +0530

    Fixed Home View UI import changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_annotation/core/app_strings.dart';
-import 'package:image_annotation/views/home_view.dart';
+import 'package:image_annotation/views/home/home_view.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,51 @@
+// @author [Sourav Ranjan Maharana]
+// @email [saurav.maharana07@gmail.com]
+// @create date 15:24:40 2025-12-22
+// @modify date 15:24:40 2025-12-22
+// @desc [File holds the UI for Home View; For now it shows a grid of images]
+
+import 'package:flutter/material.dart';
+import 'package:image_annotation/core/app_image_paths.dart';
+import 'package:image_annotation/core/app_strings.dart';
+import 'package:image_annotation/views/image_viewer/image_view.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppStrings.imageAnnotation),
+        centerTitle: false,
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: AppImagePaths.listOfImages.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+        ),
+        itemBuilder: (context, index) {
+          final String imgPath = AppImagePaths.listOfImages[index];
+          return GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute<void>(
+                builder: (context) => ImageView(imagePath: imgPath),
+              ),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Hero(
+                tag: imgPath,
+                child: Image.asset(imgPath, fit: BoxFit.cover),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
