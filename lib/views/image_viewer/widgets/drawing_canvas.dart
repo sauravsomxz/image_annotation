c19e0021885beb93052f042f37e68540288adfa7
sauravsomxz <saurav.maharana07@gmail.com>
2025-12-26 14:34:33 +0530

    Pass imageRect and imageSize to annotation logic for robust coordinate mapping and fixed annotation placement

--- lib/views/image_viewer/widgets/drawing_canvas.dart
+++ lib/views/image_viewer/widgets/drawing_canvas.dart
@@ -1,40 +1,51 @@
-// @author [Sourav Ranjan Maharana]
-// @email [saurav.maharana07@gmail.com]
-// @create date 2025-12-24
-// @desc [Widget for drawing canvas overlay on image]
-
 import 'package:flutter/material.dart';
 import 'package:image_annotation/painters/shape_painter.dart';
 import 'package:image_annotation/viewmodels/annotation_tools_vm.dart';
 import 'package:provider/provider.dart';
 
+// @author [Sourav Ranjan Maharana]
+// @email [saurav.maharana07@gmail.com]
 class DrawingCanvas extends StatelessWidget {
-  const DrawingCanvas({super.key});
+  final Rect imageRect;
+  final Size imageSize;
+
+  const DrawingCanvas({
+    super.key,
+    required this.imageRect,
+    required this.imageSize,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Consumer<AnnotationToolsVM>(
       builder: (context, viewModel, child) {
+        Offset toImageSpace(Offset local) {
+          if (!imageRect.contains(local)) return Offset.zero;
+          return Offset(
+            (local.dx - imageRect.left) * imageSize.width / imageRect.width,
+            (local.dy - imageRect.top) * imageSize.height / imageRect.height,
+          );
+        }
+
         final customPaint = CustomPaint(
           painter: ShapePainter(
             shapes: viewModel.shapes,
             currentShape: viewModel.currentShape,
+            imageRect: imageRect,
+            imageSize: imageSize,
           ),
           child: Container(),
         );
-
-        // Only consume gesture events when a tool is selected
         if (viewModel.selectedTool != null) {
           return GestureDetector(
             onPanStart: (details) =>
-                viewModel.onDrawStart(details.localPosition),
+                viewModel.onDrawStart(toImageSpace(details.localPosition)),
             onPanUpdate: (details) =>
-                viewModel.onDrawUpdate(details.localPosition),
+                viewModel.onDrawUpdate(toImageSpace(details.localPosition)),
             onPanEnd: (details) => viewModel.onDrawEnd(),
             child: customPaint,
           );
         }
-        // When no tool is selected, ignore pointer events to allow InteractiveViewer to work
         return IgnorePointer(child: customPaint);
       },
     );
