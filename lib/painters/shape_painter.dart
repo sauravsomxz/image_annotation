6b6b994063d559fbedfaea992ae74dba3f4ec548
sauravsomxz <saurav.maharana07@gmail.com>
2025-12-26 14:34:02 +0530

    Map annotation points from image coordinates to display coordinates for correct rendering at any zoom level

--- lib/painters/shape_painter.dart
+++ lib/painters/shape_painter.dart
@@ -9,8 +9,15 @@ import 'package:image_annotation/models/drawable_shape.dart';
 class ShapePainter extends CustomPainter {
   final List<DrawableShape> shapes;
   final DrawableShape? currentShape;
+  final Rect imageRect;
+  final Size imageSize;
 
-  ShapePainter({required this.shapes, this.currentShape});
+  ShapePainter({
+    required this.shapes,
+    this.currentShape,
+    required this.imageRect,
+    required this.imageSize,
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -25,6 +32,13 @@ class ShapePainter extends CustomPainter {
     }
   }
 
+  Offset _toDisplay(Offset imageOffset) {
+    return Offset(
+      imageRect.left + imageOffset.dx * imageRect.width / imageSize.width,
+      imageRect.top + imageOffset.dy * imageRect.height / imageSize.height,
+    );
+  }
+
   void _drawShape(Canvas canvas, DrawableShape shape) {
     final paint = Paint()
       ..color = shape.color
@@ -32,13 +46,27 @@ class ShapePainter extends CustomPainter {
       ..style = PaintingStyle.stroke;
 
     if (shape is CircleShape) {
-      canvas.drawCircle(shape.center, shape.radius, paint);
+      canvas.drawCircle(
+        _toDisplay(shape.center),
+        shape.radius * imageRect.width / imageSize.width,
+        paint,
+      );
     } else if (shape is RectangleShape) {
-      canvas.drawRect(Rect.fromPoints(shape.topLeft, shape.bottomRight), paint);
+      canvas.drawRect(
+        Rect.fromPoints(
+          _toDisplay(shape.topLeft),
+          _toDisplay(shape.bottomRight),
+        ),
+        paint,
+      );
     } else if (shape is FreehandShape) {
       if (shape.points.isNotEmpty) {
         for (int i = 0; i < shape.points.length - 1; i++) {
-          canvas.drawLine(shape.points[i], shape.points[i + 1], paint);
+          canvas.drawLine(
+            _toDisplay(shape.points[i]),
+            _toDisplay(shape.points[i + 1]),
+            paint,
+          );
         }
       }
     }
@@ -47,6 +75,8 @@ class ShapePainter extends CustomPainter {
   @override
   bool shouldRepaint(ShapePainter oldDelegate) {
     return oldDelegate.shapes != shapes ||
-        oldDelegate.currentShape != currentShape;
+        oldDelegate.currentShape != currentShape ||
+        oldDelegate.imageRect != imageRect ||
+        oldDelegate.imageSize != imageSize;
   }
 }
